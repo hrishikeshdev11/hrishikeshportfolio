aa14082de301a4bab07d400e290de9ece0ca1695
HRISHIKESH <100159729+HRISHIKESHB11@users.noreply.github.com>
2022-09-06 12:18:11 +0530

    Video Audio Updated

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
-
+const fades= document.querySelectorAll(".fade-in");
 const toggleNavbar = ()=>{
-    nav_header.classList.toggle('active');
+    nav_header.classList.toggle("active");
 };
 
-mobile_nav.addEventListener('click',()=> toggleNavbar());
+mobile_nav.addEventListener("click",()=> toggleNavbar());
+
+const appearOption={};
+const appearOnScroll = new IntersectionObserver(
+    function(entries,appearOnScroll){
+        entries.forEach(entry =>{
+            if(!entry.isIntersecting){
+                return;
+            }else{
+                entry.target.classList.add("appear");
+                appearOnScroll.unobserve(entry.target);
+            }
+        })
+},appearOption);
+
+fades.forEach(fader=>{
+    appearOnScroll.observe(fader);
+});
